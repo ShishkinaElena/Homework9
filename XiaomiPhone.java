b1c0443366efae39cc560b382204a748cd9d461f
ShishkinaElena <148252907+ShishkinaElena@users.noreply.github.com>
2023-11-02 20:19:41 +0400

    Add files via upload

---
+++ XiaomiPhone.java
@@ -0,0 +1,27 @@
+package Phone;
+class XiaomiPhone<T> extends Phone<T>{
+    private T number;
+    private String model;
+    private double weight;
+
+    private String abonent;
+
+
+    public XiaomiPhone(T number, String model, double weight, String abonent) {
+        super();
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+        this.abonent = abonent;
+    }
+
+    @Override
+    public void info() {
+        System.out.printf("Number: %s \tModel: %s Weight: %.2f\n", number, model, weight);
+    }
+
+    @Override
+    public void receiveCall() {
+        System.out.println( "Abonent: \t" + number + " \t" + abonent);
+    }
+}
